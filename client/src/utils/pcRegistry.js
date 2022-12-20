a9142d8f959a3bad7a02dd8065849bdb9d3a94fa
bereky <bereketlemma4@gmail.com>
2022-12-21 01:16:08 +0300

    add scanner registry utility

---
+++ client/src/utils/pcRegistry.js
@@ -0,0 +1,18 @@
+const pcRegistry =  [
+    {
+        name: "Bereket Lemma",
+        id: '123',
+        department: 'Computer Science',
+        pc_model: "HP Elitebook 840",
+        pc_serial: '5CG7171RWH'
+    },
+    {
+        name: "Bereket King",
+        id: 'RU0368/12',
+        department: 'Computer Science',
+        pc_model: "HP Elitebook 840",
+        pc_serial: '5CG7171RWH'
+    }
+]
+
+export default pcRegistry
