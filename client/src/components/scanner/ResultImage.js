2624008811b85b8e9bc9196bc53fadd7f2721051
bereky <bereketlemma4@gmail.com>
2022-12-21 00:12:52 +0300

    used image asset from props

--- client/src/components/scanner/ResultImage.js
+++ client/src/components/scanner/ResultImage.js
@@ -1,8 +1,8 @@
-const ResultImage = () => {
+const ResultImage = (props) => {
     return ( 
         <div className="bg-slate-200 w-2/5 h-auto flex justify-center items-center rounded-md py-5">
             <div className="bg-slate-100 w-72 h-72 flex justify-center items-center px-1 rounded-full ">
-                <img src="https://api.lorem.space/image/face?w=450&h=450" alt="" className="rounded-full w-full"/>
+                <img src={props.image} alt="" className="rounded-full w-full"/>
             </div>
         </div>
      );
