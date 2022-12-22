2385b914d1de09548d86cfb1506d88d34da4eee9
bereky <bereketlemma4@gmail.com>
2022-12-22 22:42:22 +0300

    add reg route component

--- client/src/components/registration/index.js
+++ client/src/components/registration/index.js
@@ -1,15 +1,34 @@
+import { SideWrapper } from "../home";
+import Brand from "../home/Brand";
+import LeftWrapper from "../home/LeftWrapper";
+import SideNav from "../home/SideNav";
+import TopMenu from "../home/TopMenu";
+import { Scanner } from "../scanner";
 import Form from "./Form";
 import Header from "./Header";
 
 const Registration = () => {
     return ( 
         <div className="w-full h-auto flex flex-col"> 
-            
             <Header />
-
             <Form />
         </div>
      );
 }
+
+const RegistrationRoute = () => {
+    return ( 
+        <>
+            <LeftWrapper>
+                <Brand />
+                <SideNav route='registration'/>
+            </LeftWrapper> 
+            <SideWrapper>
+                <TopMenu />
+                <Registration />
+            </SideWrapper>
+        </>
+     );
+}
  
-export default Registration;
+export { Registration, RegistrationRoute };
