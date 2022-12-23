ac7ff57327df14f2453aaa9f42ee8765e118787b
bereky <bereketlemma4@gmail.com>
2022-12-24 02:37:40 +0300

    remove route and wrap with sidewrapper component

--- client/src/components/update/index.js
+++ client/src/components/update/index.js
@@ -1,42 +1,27 @@
-import NotFound from './NotFound';
 import Header from './Header';
 import Search from './Search';
-import UpdateData from './UpdateData';
-import LeftWrapper from '../home/LeftWrapper';
-import Brand from '../home/Brand';
-import SideNav from '../home/SideNav';
 import { SideWrapper } from '../home';
-import TopMenu from '../home/TopMenu';
+
+import NotFound from './NotFound';
+import UpdateData from './UpdateData';
 
 const Update = () => {
     return ( 
-        <div className="w-full h-auto flex flex-col"> 
-        
-            <Header />
+        <SideWrapper>
+            <div className="w-full h-auto flex flex-col"> 
+            
+                <Header />
 
-            <Search />
+                <Search />
 
-            {/* <UpdateData /> */}
+                {/* <UpdateData /> */}
 
-            {/* <NotFound /> */}
+                {/* <NotFound /> */}
 
-        </div>
+            </div>
+        </SideWrapper>
      );
 }
 
-const UpdateRoute = () => {
-    return ( 
-        <>
-            <LeftWrapper>
-                <Brand />
-                <SideNav route='update'/>
-            </LeftWrapper> 
-            <SideWrapper>
-                <TopMenu />
-                <Update />
-            </SideWrapper>
-        </>
-     );
-}
  
-export { Update, UpdateRoute };
+export { Update };
