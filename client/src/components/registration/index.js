d0feee67fc689d14e60cf93d4d80130ae5c92e89
bereky <bereketlemma4@gmail.com>
2022-12-24 02:34:12 +0300

    remove route component and wrap everything with common parent component

--- client/src/components/registration/index.js
+++ client/src/components/registration/index.js
@@ -1,34 +1,16 @@
 import { SideWrapper } from "../home";
-import Brand from "../home/Brand";
-import LeftWrapper from "../home/LeftWrapper";
-import SideNav from "../home/SideNav";
-import TopMenu from "../home/TopMenu";
-import { Scanner } from "../scanner";
 import Form from "./Form";
 import Header from "./Header";
 
 const Registration = () => {
     return ( 
-        <div className="w-full h-auto flex flex-col"> 
-            <Header />
-            <Form />
-        </div>
-     );
-}
-
-const RegistrationRoute = () => {
-    return ( 
-        <>
-            <LeftWrapper>
-                <Brand />
-                <SideNav route='registration'/>
-            </LeftWrapper> 
-            <SideWrapper>
-                <TopMenu />
-                <Registration />
-            </SideWrapper>
-        </>
+        <SideWrapper>
+            <div className="w-full h-auto flex flex-col"> 
+                <Header />
+                <Form />
+            </div>
+        </SideWrapper>
      );
 }
  
-export { Registration, RegistrationRoute };
+export { Registration };
