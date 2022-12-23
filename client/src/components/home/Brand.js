4b5edca7b2dba3fd1a4e19638be5a7bc380690f8
bereky <bereketlemma4@gmail.com>
2022-12-24 02:38:52 +0300

    add conditional for text rendering from redux store

--- client/src/components/home/Brand.js
+++ client/src/components/home/Brand.js
@@ -1,14 +1,15 @@
 import ComputerIcon from '@mui/icons-material/Computer';
 
-const Brand = () => {
+const Brand = ({collapseState}) => {
+
     return ( 
-        <div className="w-full h-auto flex flex-col">
+        <div className={`${collapseState? 'w-24' : 'w-full'} h-auto flex flex-col`}>
             <div className="w-full h-32 flex justify-center items-center"> 
                 <div className="w-full h-full flex justify-center items-center flex-row">
                     <div className="w-16  h-20 flex justify-center items-center"> 
                         <ComputerIcon  fontSize="large"/>
                     </div>
-                    <div className="w-2/4 h-20 flex justify-start items-center">
+                    <div className={`w-2/4 h-20 flex justify-start items-center ${collapseState? 'hidden' : 'flex'}`}>
                         <h1 className="w-3/4 text-4xl font-extrabold">PCRS</h1>
                     </div>
                 </div>
