bab3abb57079ab13c7a93d5a500bd6dfdb321479
bereky <bereketlemma4@gmail.com>
2022-12-24 02:39:40 +0300

    remove unused state

--- client/src/components/home/LeftWrapper.js
+++ client/src/components/home/LeftWrapper.js
@@ -1,8 +1,7 @@
 
 const LeftWrapper = ({children}) => {
-
     return ( 
-        <div className="w-1/5 bg-slate-300 border-solid border-r-slate-200 border-r-2"> 
+        <div className="w-auto bg-slate-300 border-solid border-r-slate-200 border-r-2"> 
             {children}
         </div>
      );
