c2850c4c9b1f13fdb30bfdec5f7cb356d3b1527d
bereky <bereketlemma4@gmail.com>
2022-12-24 02:27:48 +0300

    add sidebar reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import scannerReducer from './features/scanner/scannerSlice'
+import sidebarReducer from './features/sidebar/sidebarSlice'
+
 
 export const store = configureStore({
   reducer: {
-    scanner: scannerReducer
+    scanner: scannerReducer,
+    sidebar: sidebarReducer
   },
 })
