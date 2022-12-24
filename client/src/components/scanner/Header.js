beed64ee78a9e8db98216ec3e787d47faceba401
bereky <bereketlemma4@gmail.com>
2022-12-24 04:42:28 +0300

    update fontsize

--- client/src/components/scanner/Header.js
+++ client/src/components/scanner/Header.js
@@ -2,10 +2,10 @@ const Header = () => {
     return ( 
         <div className="w-full h-20 flex flex-col justify-end items-center px-5 bg-slate-100 shadow-sm">
             <div className="w-full h-12 flex justify-end items-center">
-                <h1 className="w-full text-3xl font-bold">Scanner</h1>
+                <h1 className="w-full text-2xl font-bold">Scanner</h1>
             </div> 
             <div className="w-full h-8 flex justify-start items-center">
-                <h1 className="w-3/4 text-md font-bold">Wed, Oct 8, 2022</h1>
+                <h1 className="w-3/4 text-sm font-bold">Wed, Oct 8, 2022</h1>
             </div>
         </div>
      );
