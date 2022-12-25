c32a4b93f24584fbb13bab6cd67074415a5986a9
bereky <bereketlemma4@gmail.com>
2022-12-25 12:48:17 +0300

    bg color change

--- client/src/components/home/TopMenu.js
+++ client/src/components/home/TopMenu.js
@@ -1,6 +1,6 @@
 const TopMenu = () => {
     return ( 
-        <div className="w-full h-12 bg-slate-300"> </div>
+        <div className="w-full h-12 bg-slate-200 rounded-b-md"> </div>
      );
 }
  
