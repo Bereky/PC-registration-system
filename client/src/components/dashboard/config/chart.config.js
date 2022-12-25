50018b4f949432d48793cf454f536c9f88c174c7
bereky <bereketlemma4@gmail.com>
2022-12-25 23:54:09 +0300

    chart lable change

--- client/src/components/dashboard/config/chart.config.js
+++ client/src/components/dashboard/config/chart.config.js
@@ -1,11 +1,11 @@
 export const data = {
     labels: [
-      'Verfied',
-      'Rejected'
+      'Entered',
+      'Exited'
     ],
     datasets: [{
       label: 'Scanned',
-      data: [71, 6],
+      data: [41, 35],
       backgroundColor: [
         '#4ADE80',
         '#F87171'
