10a4c51118167657baeccc3efc3959e7be498854
bereky <bereketlemma4@gmail.com>
2022-12-25 12:59:11 +0300

    add avatar

--- client/src/utils/pcRegistry.js
+++ client/src/utils/pcRegistry.js
@@ -2,6 +2,7 @@ const pcRegistry =  [
     {
         name: "Bereket Lemma",
         id: '123',
+        avatar: 'https://api.lorem.space/image/face?w=250&h=250',
         department: 'Computer Science',
         pc_model: "HP Elitebook 840",
         pc_serial: '5CG7171RWH'
@@ -9,6 +10,7 @@ const pcRegistry =  [
     {
         name: "Bereket King",
         id: 'RU0368/12',
+        avatar: 'https://api.lorem.space/image/face?w=250&h=250',
         department: 'Computer Science',
         pc_model: "HP Elitebook 840",
         pc_serial: '5CG7171RWH'
