ac4bb020bf611fa80db8cb533042bdc6e8fa7058
bereky <bereketlemma4@gmail.com>
2022-12-26 15:46:24 +0300

    add sub heading and reformat the top menu

--- client/src/components/scanner/Header.js
+++ client/src/components/scanner/Header.js
@@ -1,11 +1,11 @@
 const Header = () => {
     return ( 
-        <div className="w-full h-20 flex flex-col justify-end items-center px-5 bg-slate-100 shadow-sm">
-            <div className="w-full h-12 flex justify-end items-center">
+        <div className="w-full h-20 flex flex-col justify-end items-center px-5 bg-slate-100 shadow-sm mb-5">
+            <div className="w-full h-10 flex justify-end items-center">
                 <h1 className="w-full text-2xl font-bold">Scanner</h1>
             </div> 
             <div className="w-full h-8 flex justify-start items-center">
-                <h1 className="w-3/4 text-sm font-bold">Wed, Oct 8, 2022</h1>
+                <h1 className="w-3/4 text-sm font-light">Monitor computers entering or leaving campus</h1>
             </div>
         </div>
      );
