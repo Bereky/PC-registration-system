290994648fa20a236157a42c46883e31023f2f9a
bereky <bereketlemma4@gmail.com>
2022-12-26 15:48:52 +0300

    remove bg and position user dropdown at the top right

--- client/src/components/home/TopMenu.js
+++ client/src/components/home/TopMenu.js
@@ -1,26 +1,23 @@
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const TopMenu = () => {
     return ( 
-        <div className="w-full h-12 bg-slate-300 rounded-b-md flex flex-row "> 
-
-            <div className="w-1/2 h-12"> 
-        
-            </div>
-
-             <div className="w-1/2 h-12 flex justify-end items-center mr-5"> 
-                <div className="w-auto h-10 flex justify-center items-center flex-row bg-slate-200 rounded-md">
+        <div className="w-ful h- bg-slate-30 rounded-b-md flex flex-row absolute z-20 right-0 "> 
+             <div className="w-full h-20 flex justify-end items-center mr-5 cursor-pointer"> 
+                <div className="w-auto w-min-48 h-11 flex justify-between items-center flex-row bg-slate-100 rounded-md outline outline-1 outline-slate-300 ">
                     <div className="w-12 h-12 flex justify-center items-center">
                         <div className="w-9 h-9 flex justify-center items-center bg-red-400 rounded-full"> 
                             <img src="https://api.lorem.space/image/face?w=160&h=160" alt="" className="rounded-full w-full"/>
                         </div> 
                     </div>
 
-                    <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-                        <label tabIndex={0} className="mb-4 w-36 text-md h-12">Abera - Gate 03<span><ArrowDropDownIcon /></span></label>
-                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-48">
-                            <li><a>Item 1</a></li>
-                            <li><a>Log out</a></li>
+                    <div className="dropdown dropdown-bottom dropdown-end ">
+                        <label tabIndex={0} className="w-36 text-md h-12 cursor-pointer">Abera - adminstrator<span><ArrowDropDownIcon /></span></label>
+                        <ul tabIndex={0} className="dropdown-content p-2 menu shadow bg-base-100 rounded-box w-48">
+                            <li><a><SettingsIcon /> Setting</a></li>
+                            <li><a><LogoutIcon /> Log out </a></li>
                         </ul>
                     </div> 
                 </div>
