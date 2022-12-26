45a46623e201d48e552f51fe30105d1e17586a97
bereky <bereketlemma4@gmail.com>
2022-12-26 16:14:42 +0300

    height change on image container

--- client/src/components/scanner/ResultImage.js
+++ client/src/components/scanner/ResultImage.js
@@ -1,8 +1,8 @@
 const ResultImage = (props) => {
     return ( 
-        <div className="bg-slate-200 w-2/5 h-auto flex justify-center items-center rounded-md py-5">
-            <div className="bg-slate-100 w-72 h-72 flex justify-center items-center px-1 rounded-full ">
-                <img src={props.image} alt="" className="rounded-full w-full"/>
+        <div className="bg-slate-200 w-2/5 h-72 flex justify-center items-center rounded-md">
+            <div className="bg-slate-100 w-64 h-64 flex justify-center items-center px-1 rounded-full ">
+                <img src={props.image} alt="profile_avatar" className="rounded-full w-full"/>
             </div>
         </div>
      );
