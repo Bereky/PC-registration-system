729324be0856fa5621c65f2eafe80fbdf683ccba
bereky <bereketlemma4@gmail.com>
2022-12-29 21:37:21 +0300

    bg and text change

--- client/src/components/scanner/NotFound.js
+++ client/src/components/scanner/NotFound.js
@@ -2,12 +2,12 @@ import CancelIcon from '@mui/icons-material/Cancel';
 
 const NotFound = () => {
     return ( 
-        <div className="w-full h-full flex justify-center items-center flex-col p-2 rounded-md shadow-md">
-            <div className="w-full bg-slate-200 h-36 flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center flex-col p-2 rounded-md shadow-sm bg-slate-100 my-3">
+            <div className="w-full h-40 flex justify-center items-center">
                 <CancelIcon sx={{'font-size': '110px'}} className="text-3xl text-red-400 rounded-full"/>
             </div>
-            <div className="w-full bg-slate-200 h-20 flex justify-center items-center">
-                <h1 className="w-auto text-4xl font-bold">PC not found</h1>
+            <div className="w-full h-20 flex justify-center items-center">
+                <h1 className="w-auto text-4xl font-bold">Not found</h1>
             </div>
         </div>
      );
