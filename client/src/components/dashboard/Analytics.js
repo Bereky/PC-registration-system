8f5f6f031e6717c35b4548542ee4583b141dc738
bereky <bereketlemma4@gmail.com>
2023-01-02 23:45:21 +0300

    separate entered component

--- client/src/components/dashboard/Analytics.js
+++ client/src/components/dashboard/Analytics.js
@@ -9,15 +9,8 @@ const Analytis = () => {
             
             <Checked />
 
-            <div className="w-72 h-32 bg-green-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
-                <div className="w-1/2 h-full  flex justify-center items-center">
-                    <SouthIcon sx={{'font-size': '90px'}} className=" text-green-400 rounded-full bg-white"/>
-                </div>
-                <div className="w-1/2 h-32 flex justify-center items-center flex-col space-y-4 text-white">
-                    <h1 className="w-5/6 text-md font-bold text-center">Entered campus</h1>
-                    <h1 className="w-3/4 text-5xl font-bold text-center">41</h1>
-                </div>
-            </div>
+            <Entered />
+            
             <div className="w-72 h-32 bg-red-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
                 <div className="w-1/2 h-full  flex justify-center items-center">
                     <NorthIcon sx={{'font-size': '90px'}} className="text-red-400 bg-white rounded-full"/>
@@ -40,6 +33,20 @@ const Checked = () => {
             <div className="w-1/2 h-32 flex justify-center items-center flex-col space-y-4 text-white">
                 <h1 className="w-3/4 text-md font-bold text-center">Total Checked</h1>
                 <h1 className="w-3/4 text-5xl font-bold text-center">76</h1>
+            </div>
+        </div>
+    );
+}
+
+const Entered = () => {
+    return (
+        <div className="w-72 h-32 bg-green-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
+            <div className="w-1/2 h-full  flex justify-center items-center">
+                <SouthIcon sx={{'font-size': '90px'}} className=" text-green-400 rounded-full bg-white"/>
+            </div>
+            <div className="w-1/2 h-32 flex justify-center items-center flex-col space-y-4 text-white">
+                <h1 className="w-5/6 text-md font-bold text-center">Entered campus</h1>
+                <h1 className="w-3/4 text-5xl font-bold text-center">41</h1>
             </div>
         </div>
     );
