79de24a3578119d809565dcadbeee6a113c995c0
bereky <bereketlemma4@gmail.com>
2023-01-02 23:53:40 +0300

    separate title/sub component

--- client/src/components/dashboard/Header.js
+++ client/src/components/dashboard/Header.js
@@ -1,14 +1,27 @@
 const Header = () => {
     return ( 
         <div className="w-full h-20 flex flex-col justify-end items-center px-5 bg-slate-100 shadow-sm mb-5">
-            <div className="w-full h-10 flex justify-end items-center">
-                <h1 className="w-full text-2xl font-bold">Dashboard</h1>
-            </div> 
-            <div className="w-full h-8 flex justify-start items-center">
-                <h1 className="w-3/4 text-sm font-bold">Welcome, here is what's happening today</h1>
-            </div>
+             
+            <Title />
+            <SubTitle />
+            
         </div>
      );
 }
  
+const Title = () => {
+    return (
+        <div className="w-full h-10 flex justify-end items-center">
+            <h1 className="w-full text-2xl font-bold">Dashboard</h1>
+        </div>
+    );
+}
+
+const SubTitle = () => {
+    return (
+        <div className="w-full h-8 flex justify-start items-center">
+            <h1 className="w-3/4 text-sm font-bold">Welcome, here is what's happening today</h1>
+        </div>
+    );
+}
 export default Header;
