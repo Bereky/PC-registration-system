a149384792aae41c7bb819bc7c178aafff1f41d4
bereky <bereketlemma4@gmail.com>
2023-01-02 23:43:48 +0300

    separate checked component

--- client/src/components/dashboard/Analytics.js
+++ client/src/components/dashboard/Analytics.js
@@ -6,15 +6,9 @@ import SouthIcon from '@mui/icons-material/ArrowDownward';
 const Analytis = () => {
     return ( 
         <div className="w-full h-40 flex justify-between items-center flex-row px-5 border-solid border-b-slate-200 border-b-2">
-            <div className="w-72 h-32 bg-blue-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
-                <div className="w-1/2 h-full  flex justify-center items-center">
-                    <SearchIcon sx={{'font-size': '90px'}} className="text-3xl p-2 bg-white text-blue-400 rounded-full"/>
-                </div>
-                <div className="w-1/2 h-32 flex justify-center items-center flex-col space-y-4 text-white">
-                    <h1 className="w-3/4 text-md font-bold text-center">Total Checked</h1>
-                    <h1 className="w-3/4 text-5xl font-bold text-center">76</h1>
-                </div>
-            </div>
+            
+            <Checked />
+
             <div className="w-72 h-32 bg-green-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
                 <div className="w-1/2 h-full  flex justify-center items-center">
                     <SouthIcon sx={{'font-size': '90px'}} className=" text-green-400 rounded-full bg-white"/>
@@ -35,6 +29,20 @@ const Analytis = () => {
             </div>
         </div>
      );
+}
+
+const Checked = () => {
+    return (
+        <div className="w-72 h-32 bg-blue-400 flex flex-row justify-center items-center rounded-xl shadow-lg">
+            <div className="w-1/2 h-full  flex justify-center items-center">
+                <SearchIcon sx={{'font-size': '90px'}} className="text-3xl p-2 bg-white text-blue-400 rounded-full"/>
+            </div>
+            <div className="w-1/2 h-32 flex justify-center items-center flex-col space-y-4 text-white">
+                <h1 className="w-3/4 text-md font-bold text-center">Total Checked</h1>
+                <h1 className="w-3/4 text-5xl font-bold text-center">76</h1>
+            </div>
+        </div>
+    );
 }
  
 export default Analytis;
